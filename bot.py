60585f85e8a8665d7965ce6f133ccfd65f779d9d
Burrito <37681398+burritosoftware@users.noreply.github.com>
2023-05-05 12:53:31 -0700

    Initial tic tac toe work

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ if os.name != "nt":
 # initializing everything
 load_dotenv()
 logger = logging.getLogger('tttt.bot')
-bot = lightbulb.BotApp(token=os.getenv('TOKEN'), prefix=os.getenv('PREFIX'), banner=None, default_enabled_guilds=(802249775201910834), intents=hikari.Intents.ALL_UNPRIVILEGED)
+bot = lightbulb.BotApp(token=os.getenv('TOKEN'), prefix=os.getenv('PREFIX'), banner=None, default_enabled_guilds=(1104130814255566899), intents=hikari.Intents.ALL_UNPRIVILEGED)
 miru.install(bot)
 
 # creating objects/connections
--- extensions/tictac.py
+++ extensions/tictac.py
@@ -5,25 +5,44 @@ import miru
 plugin = lightbulb.Plugin("Tic Tac Trig Triple")
 
 class TicTacBoard:
-    rows = 9
-    cols = 9
-    array = [ ["E" for x in range (cols)] for y in range (rows)]
+    # E = empty space
+    # X = Player 1 Claim
+    # Y = Player 2 Claim
+    # Z = Player 3 Claim
+    def __init__(self):
+        self.array = [ ["E" for x in range (9)] for y in range (9)]
 
     def formatBoard(self):
         formattedBoard = ""
-        for row in array:
+        for row in self.array:
             for value in row:
                 if value == "E":
                     formattedBoard += "⬛"
+                elif value == "X":
+                    formattedBoard += "🔷"
+                elif value == "Y":
+                    formattedBoard += "🔶"
+                elif value == "Z":
+                    formattedBoard += "♦️"
             formattedBoard += "\n"
         return hikari.Embed(title="Game Board", description=formattedBoard)
+    
+    def claimSpace(self, x, y, p):
+        if self.array[x][y] != "E":
+            return False
+        self.array[x][y] = p
+        return True
 
 @plugin.command
 @lightbulb.command("start", description="Start a new tic-tac-trig-triple game between 2 other players!")
 @lightbulb.implements(lightbulb.SlashCommand)
 async def start(ctx: lightbulb.Context) -> None:
     board = TicTacBoard();
-    ctx.respond("")
+    await ctx.respond(embed=board.formatBoard())
+    board.claimSpace(2,3,"X")
+    board.claimSpace(3,3,"Y")
+    board.claimSpace(4,3,"Z")
+    await ctx.respond(embed=board.formatBoard())
 
 def load(bot):
     bot.add_plugin(plugin)
