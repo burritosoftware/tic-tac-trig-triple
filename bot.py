1eb624118e99aabd45e240a17c30ae90e6fc8b45
Burrito <37681398+burritosoftware@users.noreply.github.com>
2023-05-14 22:24:17 -0700

    Remove unnecessary dependencies

--- bot.py
+++ bot.py
@@ -6,9 +6,7 @@ import hikari
 import lightbulb
 import miru
 # miscellaneous
-import aiohttp
 import logging
-import dataset
 
 # uvloop for performance on UNIX systems
 if os.name != "nt":
@@ -18,27 +16,14 @@ if os.name != "nt":
 # initializing everything
 load_dotenv()
 logger = logging.getLogger('tttt.bot')
-bot = lightbulb.BotApp(token=os.getenv('TOKEN'), prefix=os.getenv('PREFIX'), banner=None, default_enabled_guilds=(1106276013522038876), intents=hikari.Intents.ALL_UNPRIVILEGED)
+bot = lightbulb.BotApp(token=os.getenv('TOKEN'), prefix=os.getenv('PREFIX'), banner=None, intents=hikari.Intents.ALL_UNPRIVILEGED)
 miru.install(bot)
 
 # creating objects/connections
 @bot.listen()
 async def on_starting(event: hikari.StartingEvent) -> None:
-    bot.d.aio_session = aiohttp.ClientSession()
-    logger.info("Created aiohttp.ClientSession")
     bot.d.logger = logger
     logger.info("Added logger to datastore")
-    bot.d.db = dataset.connect(os.getenv('DATABASE'), engine_kwargs=dict(connect_args={'check_same_thread': False}))
-    logger.info(f"Connected to database {os.getenv('DATABASE')}")
-
-# disposing objects/connections
-@bot.listen()
-async def on_stopping(event: hikari.StoppingEvent) -> None:
-    bot.d.db.close()
-    logger.info("Closed database connection")
-    # Close the ClientSession
-    await bot.d.aio_session.close()
-    logger.info("Closed aiohttp.ClientSession")
 
 bot.load_extensions_from("./extensions/", must_exist=True)
 
